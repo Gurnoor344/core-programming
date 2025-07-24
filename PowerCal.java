ebcc0c1d35273eaaad398635080b2789a3e68e30
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-24 15:13:51 +0530

    PowerCal Question

---
+++ PowerCal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class PowerCal{
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		double base = sc.nextInt();
+		double exponenet = sc.nextInt();
+		double result = Math.pow(base,exponenet);
+		System.out.println(result);
+	}
+}
+		
