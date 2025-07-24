6083133154df69971458e796c55753296b7595e6
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-24 12:30:45 +0530

    WelcomeMessage

---
+++ WelcomeMessage.java
@@ -0,0 +1,9 @@
+//Write a program that prints "Welcome to Bridgelabz!" to the screen.
+
+class WelcomeMessage {
+	public static void main(String[] args){
+		System.out.println("Welcome to Bridgelabz!");
+	}
+}
+
+
