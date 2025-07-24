12f23971574872d74896220178ede18884fe38b3
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-24 12:46:10 +0530

    Area Question

---
+++ Radius.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Radius{
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		int radius=sc.nextInt();
+		double Area = 3.14 * radius * radius;
+		System.out.println(Area);
+	}
+}
+		
