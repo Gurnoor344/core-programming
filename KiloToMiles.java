ca0a1f629b91acec4105647a5da54958fbf768e2
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-24 15:22:29 +0530

    KiloToMiles Question

---
+++ KiloToMiles.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class KiloToMiles{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int kilometers = sc.nextInt();
+	double miles = kilometers * 0.621371;
+		System.out.println(miles);
+	}
+}
