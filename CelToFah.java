2f7090e36b9e936a52868d142c09d904456e510b
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-26 15:04:21 +0530

    Remove unwanted files from javacontrols branch

--- CelToFah.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class CelToFah{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int celcius = sc.nextInt();
-		float Fahrenheit = (celcius * 9/5) + 32;
-		System.out.println(Fahrenheit);
-	}
-}
--- WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-//Write a program that prints "Welcome to Bridgelabz!" to the screen.
-
-class WelcomeMessage {
-	public static void main(String[] args){
-		System.out.println("Welcome to Bridgelabz!");
-	}
-}
-
-
