2b39f2a675b054b2b64f1cdbfae9ddea4cd82087
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-26 15:25:42 +0530

    Divisible

---
+++ Divisible.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Divisible{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if(number%5==0){
+			System.out.println("Yes it is divisible");
+		}
+		else {
+			System.out.println("No it is not divisible");
+		}
+	}
+}
+		
+	
